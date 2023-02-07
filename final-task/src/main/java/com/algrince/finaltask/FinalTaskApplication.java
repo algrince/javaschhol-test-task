1aacc18fc3fa8d45ce92cf8ac5027be705e76f0e
Ekaterina Mitiashkina <algrince@gmail.com>
2023-02-07 18:34:32 +0100

    init with spring boot

---
+++ final-task/src/main/java/com/algrince/finaltask/FinalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.algrince.finaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTaskApplication.class, args);
+	}
+
+}
