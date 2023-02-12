3f502542acbf3b595ddb4681e4c63d1a5242be84
Ekaterina Mitiashkina <algrince@gmail.com>
2023-02-12 11:04:47 +0100

    fix reg form errors

--- src/main/java/com/algrince/finaltask/models/Client.java
+++ src/main/java/com/algrince/finaltask/models/Client.java
@@ -3,17 +3,16 @@ package com.algrince.finaltask.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "clients")
-@Getter
-@Setter
+@Data
 public class Client {
     @Id
     @Column(name = "id")
@@ -31,7 +30,7 @@ public class Client {
     private String surname;
 
     @Column(name = "date_of_birth")
-    @NotEmpty(message = "Date of birth should not be empty")
+    @NotNull(message = "Date of birth should not be empty")
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date dateOfBirth;
@@ -56,20 +55,4 @@ public class Client {
 //    @NotEmpty
     private boolean isDeleted;
 
-    public Client() {
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                ", isDeleted=" + isDeleted +
-                '}';
-    }
 }
