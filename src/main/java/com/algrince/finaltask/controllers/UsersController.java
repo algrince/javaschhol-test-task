0077556601fb73eca5911373e2b4df87d698b1ee
Ekaterina Mitiashkina <algrince@gmail.com>
2023-02-18 20:54:52 +0100

    add crossorigin (user list not showing fix)

--- src/main/java/com/algrince/finaltask/controllers/UsersController.java
+++ src/main/java/com/algrince/finaltask/controllers/UsersController.java
@@ -4,6 +4,7 @@ package com.algrince.finaltask.controllers;
 import com.algrince.finaltask.models.User;
 import com.algrince.finaltask.services.UsersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsersController {
 
     private final UsersService usersService;
