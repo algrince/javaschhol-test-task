fa4985b1689e7a48fb22e397f00d9d247a5645f7
Ekaterina Mitiashkina <algrince@gmail.com>
2023-03-20 19:49:51 +0100

    add cart

--- angularclient/src/app/app.component.ts
+++ angularclient/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { AuthenticationService } from './service/authentication.service';
+import { CartService } from './service/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,47 @@ import { AuthenticationService } from './service/authentication.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    @ViewChildren("subTotalWrap") subTotalItems: QueryList<ElementRef>;
+    @ViewChildren("subTotalWrap_existing") subTotalItems_existing: QueryList<ElementRef>;
 
   title: string;
+  isHidden = false;
+  items = [];
 
-  constructor(public authenticationService: AuthenticationService) {
+  constructor(public authenticationService: AuthenticationService,
+  private cartService: CartService) {
     this.title = 'The Store';
   }
 
   ngOnInit() {
+    this.cartService.loadCart();
+    this.items = this.cartService.getItems();
+  }
+
+    removeFromCart(item) {
+      this.cartService.removeItem(item);
+      this.items = this.cartService.getItems();
+    }
+
+  get total() {
+    return this.items.reduce(
+      (sum, x) => ({
+        qtyTotal: 1,
+        price: sum.price + x.qtyTotal * x.price
+      }),
+      { qtyTotal: 1, price: 0 }
+    ).price;
+  }
+
+  changeSubtotal(item, index) {
+    const qty = item.qtyTotal;
+    const amt = item.price;
+    const subTotal = amt * qty;
+
+    this.subTotalItems.toArray()[
+      index
+    ].nativeElement.innerHTML = subTotal;
+    this.cartService.saveCart();
   }
 
 }
--- angularclient/src/app/view/product/product-detail/product-detail.component.ts
+++ angularclient/src/app/view/product/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Product } from '../../../model/product';
 import { ProductService } from '../../../service/product.service';
 import { ImageService } from '../../../service/image.service';
+import { CartService } from '../../../service/cart.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -15,12 +16,14 @@ export class ProductDetailComponent implements OnInit {
     product: Product;
     productId: number;
     imageSrc: any;
+    items = [];
 
     constructor(
         private productService: ProductService,
         private router: Router,
         private route: ActivatedRoute,
         private imageService: ImageService,
+        private cartService: CartService,
         private sanitizer: DomSanitizer) {
             this.product = new Product();
         }
@@ -39,6 +42,14 @@ export class ProductDetailComponent implements OnInit {
         this.imageService.getImage({id: this.productId})
             .subscribe(data =>
                 {this.imageSrc = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${data}`);});
+    }
+
+    addToCart(item) {
+        if (!this.cartService.itemInCart(item)) {
+          item.qtyTotal = 1;
+          this.cartService.addToCart(item);
+          this.items = [...this.cartService.getItems()];
+        }
     }
 
 }
