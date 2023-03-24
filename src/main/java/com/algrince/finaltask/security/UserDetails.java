e682b6de11a2c0c15f117d2dd7499149b95c9e9f
Ekaterina Mitiashkina <algrince@gmail.com>
2023-03-24 02:18:53 +0100

    fix role access

--- src/main/java/com/algrince/finaltask/security/UserDetails.java
+++ src/main/java/com/algrince/finaltask/security/UserDetails.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.Collection;
 import java.util.Collections;
 
+
 public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
     private final User user;
 
--- src/main/java/com/algrince/finaltask/utils/AccessValidator.java
+++ src/main/java/com/algrince/finaltask/utils/AccessValidator.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Component
@@ -17,15 +20,15 @@ public class AccessValidator {
             Principal principal,
             User foundUser) {
 
-        String role = null;
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            for (GrantedAuthority authority : userDetails.getAuthorities()) {
-                role = authority.getAuthority();
-            }
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = new ArrayList<>();
+        for (GrantedAuthority authority : authorities) {
+            roles.add(authority.getAuthority());
         }
+
+        String role = roles.get(0);
 
         return (Objects.equals(role, "ROLE_EMPLOYEE") ||
                 Objects.equals(role, "ROLE_ADMIN") ||
